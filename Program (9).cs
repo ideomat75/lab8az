3e535ff1c905e368f00a133ded1bd142968c2776
ideomat75 <90716549+ideomat75@users.noreply.github.com>
2022-11-18 21:03:36 +0300

    Add files via upload

---
+++ Program (9).cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using lab08;
+
+namespace des
+{
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream fs = new FileStream("C:\\Users\\andre\\source\\repos\\lab08\\lab08\\bin\\Debug\\res.xml", FileMode.Open);
+            XmlSerializer serializer = new XmlSerializer(typeof(Animal));
+            Animal res = (Animal)serializer.Deserialize(fs);
+
+            Console.WriteLine($"{res.Name}\t{res.Country}\t{res.ClassificationAnimal}\t{res.HideFromOtherAnimals}");
+        }
+    }
+}
