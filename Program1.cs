6227777a931a61fefee3e3b326c25fc80691a95c
ideomat75 <90716549+ideomat75@users.noreply.github.com>
2022-11-18 21:06:45 +0300

    Add files via upload

---
+++ Program1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string filename, dir, arch;
+            
+            Console.WriteLine("Enter file name");
+            filename = Console.ReadLine();
+
+            Console.WriteLine("Enter dir");
+            dir = Console.ReadLine();
+
+            Console.WriteLine("Arch(a) or no");
+            arch = Console.ReadLine();
+
+            string[] all = Directory.GetFiles(dir, filename, SearchOption.AllDirectories);
+
+            if (all.Length == 0)
+                Console.WriteLine("No file");
+
+            if (all.Length > 1)
+                foreach(var f in all)
+                    Console.WriteLine(f);
+
+            if (all.Length == 1)
+            {
+                FileStream fs = new FileStream(all[0], FileMode.Open);
+
+                if (arch == "a")
+                {
+                    Console.WriteLine("Enter full file name");
+                    string resfile = Console.ReadLine();
+
+                    GZipStream gZipStream = new GZipStream(File.Create(resfile), CompressionMode.Compress);
+                    fs.CopyTo(gZipStream);
+                }
+                else
+                {
+                    for (int i = 0; i < fs.Length; i++)
+                        Console.Write((char)fs.ReadByte());
+                }
+            }
+        }
+    }
+}
---
+++ Program3.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace lab08
+{
+    public enum eClassificationAnimal
+    {
+        Herbivores,
+        Carnivores,
+        Omnivores
+    }
+
+    public enum eFavouriteFood
+    {
+        Meat,
+        Plants,
+        Everything
+    }
+    public class Animal
+    {
+        public string Name { get; set; }
+
+        public string Country { get; set; }
+
+        public string HideFromOtherAnimals { get; set; }
+
+        public string WhatAnimal { get; set; }
+
+        public eClassificationAnimal ClassificationAnimal = eClassificationAnimal.Omnivores;
+
+
+        public Animal() { }
+
+        public void Deconstruct() { }
+
+        public eClassificationAnimal GetClassificationAnimal()
+        {
+            return ClassificationAnimal;
+        }
+
+        public eFavouriteFood GetFavouriteFood()
+        {
+            return eFavouriteFood.Everything;
+        }
+
+        public string SayHello()
+        {
+            return "hello";
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal animal = new Animal();
+            animal.Name = "bob";
+            animal.Country = "moscow";
+            animal.WhatAnimal = "pig";
+            animal.HideFromOtherAnimals = "no";
+
+            XmlSerializer xml = new XmlSerializer(typeof(Animal));
+            FileStream fs = new FileStream("res.xml", FileMode.OpenOrCreate);
+
+            xml.Serialize(fs, animal);
+        }
+    }
+}
